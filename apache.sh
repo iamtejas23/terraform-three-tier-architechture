2076675efc9a338e2113daaf5e3a119eb577e260
Tejas Mane <60383103+iamtejas23@users.noreply.github.com>
2024-08-24 23:31:17 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-amazon-linux-extras install -y docker
+# Switch to root user
+sudo su -
+
+# Update the system and install necessary packages
+yum update -y
+yum install -y git docker
+
+# Start Docker service
 service docker start
-usermod -a -G docker ec2-user
-docker run -d -p 80:80 iamtejas23/e-kart:latest
+chkconfig docker on
+
+# Clone the React app repository from GitHub
+git clone https://github.com/iamtejas23/personal-portfolio.git /root/app
+
+# Navigate to the app directory
+cd /root/app
+
+# Build and run the Docker container
+docker build -t react-app .
+docker run -d -p 80:3000 react-app
+
+# Optional: Clean up the Docker images to save space
+docker image prune -f
