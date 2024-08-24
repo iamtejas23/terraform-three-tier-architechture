1b12bbb998e2a2fd220e7ddeb34fb6e9dc606fe4
Tejas Mane <60383103+iamtejas23@users.noreply.github.com>
2024-08-24 23:19:12 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,35 +1,5 @@
-#! /bin/bash
-# Use an official Node.js image to build the React app
-FROM node:18 AS build
-
-# Set the working directory
-WORKDIR /app
-
-# Install git
-RUN apt-get update && apt-get install -y git
-
-# Clone the GitHub repository (replace with your repo URL)
-RUN git clone https://github.com/iamtejas23/personal-portfolio.git .
-
-# Install the dependencies
-RUN npm install
-
-# Build the React app for production
-RUN npm run build
-
-# Use an official Nginx image to serve the app
-FROM nginx:alpine
-
-# Copy the build files from the previous stage to the Nginx public directory
-COPY --from=build /app/build /usr/share/nginx/html
-
-# Expose port 80 to the outside world
-EXPOSE 80
-
-# Start Nginx
-CMD ["nginx", "-g", "daemon off;"]
-
-
-docker build -t react-nginx-app .
-
-docker run -p 80:80 react-nginx-app
+#!/bin/bash
+amazon-linux-extras install -y docker
+service docker start
+usermod -a -G docker ec2-user
+docker run -d -p 80:80 iamtejas23/e-kart:latest
