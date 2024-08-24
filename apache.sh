93f86675d1ffbd5ce7a29559bcfe0d1d2fcaa530
Tejas Mane <60383103+iamtejas23@users.noreply.github.com>
2024-08-24 23:38:43 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,24 +1,7 @@
-#!/bin/bash
-# Switch to root user
-sudo su -
-
-# Update the system and install necessary packages
-yum update -y
-yum install -y git docker
-
-# Start Docker service
-service docker start
-chkconfig docker on
-
-# Clone the React app repository from GitHub
-git clone https://github.com/iamtejas23/personal-portfolio.git /root/app
-
-# Navigate to the app directory
-cd /root/app
-
-# Build and run the Docker container
-docker build -t react-app .
-docker run -d -p 80:3000 react-app
-
-# Optional: Clean up the Docker images to save space
-docker image prune -f
+#! /bin/bash
+yum install httpd git -y
+systemctl start httpd
+systemctl status httpd
+cd /var/www/html
+git clone https://github.com/iamtejas23/biz.git
+mv biz/* .
